f2f2070e8f869ce879572495961b3d0fe1c384fd
arnab003 <arnab.saha@fusioncharts.com>
2016-09-15 00:52:03 +0530

    Display error message on login failure.

--- webAppNg/loginCtrl.js
+++ webAppNg/loginCtrl.js
@@ -7,7 +7,9 @@ app.controller('loginCtrl', function ($scope, userSvc, $location) {
         userSvc.login(username, password)
         .then(function (response) {
             if (response.status === 401) {
-            	$scope.message = 'Invalid user';
+            	$scope.message = '*Invalid username or password!';
+                $scope.username = '';
+                $scope.password = '';
             }
             else {
             	window.localStorage.token = response.result.token;
